6202a7b10ea2cfc013a47616a2b3992f9b4b323d
Gustavo Barron <cicloid@42fu.com>
2017-03-17 00:29:13 -0600

    Added modal Vim window managament to Hammerspoon

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -12,8 +12,37 @@ hs.grid.MARGINY    = 0
 -- Set window animation off. It's much smoother.
 hs.window.animationDuration = 0
 
+
+
+
+
+-- Modal ViM
+local function keyCode(key)
+  return function() hs.eventtap.keyStroke({}, key) end
+end
+
+hs.hotkey.bind({"cmd", "alt"}, 'h', keyCode('left') ,  nil,   keyCode('left'))
+hs.hotkey.bind({"cmd", "alt"}, 'j', keyCode('down') ,  nil,   keyCode('down') )
+hs.hotkey.bind({"cmd", "alt"}, 'k', keyCode('up')   ,  nil,   keyCode('up') )
+hs.hotkey.bind({"cmd", "alt"}, 'l', keyCode('right'),  nil,   keyCode('right') )
+
+modalVimMode = hs.hotkey.modal.new({"cmd", "alt"}, '\\', "Vimificating")
+
+modalVimMode:bind({}, 'h', keyCode('left') ,  nil, keyCode('left')  )
+modalVimMode:bind({}, 'j', keyCode('down') ,  nil, keyCode('down')  )
+modalVimMode:bind({}, 'k', keyCode('up')   ,  nil, keyCode('up')    )
+modalVimMode:bind({}, 'l', keyCode('right'),  nil, keyCode('right') )
+
+function modalVimMode:exited()
+  -- hs.screen.primaryScreen():setGamma({alpha=1.0,red=0.0,green=0.0,blue=0.0},{blue=1.0,green=1.0,red=1.0})
+  hs.alert.show("No ViM mode")
+end
+
+modalVimMode:bind({}, 'escape', function() modalVimMode:exit() end)
+
 -- Init modalWindowManager
 modalWindowManager = hs.hotkey.modal.new({"cmd", "alt"}, "`", "Entering Modal Mode")
+
 
 function modalWindowManager:entered()
   -- hs.screen.primaryScreen():setGamma({alpha=1.0,red=0.0,green=0.0,blue=0.0},{blue=0.5,green=0.5,red=0.5})
