4f28c3873847397c18abcc634fbea86d58efda94
Gustavo Barron <gustavo@42fu.com>
2022-02-18 15:21:59 -0600

    Initial config for neovim

---
+++ nvim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,149 @@
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+return require("packer").startup {
+  function(use)
+    -- Packer can manage itself
+    use "wbthomason/packer.nvim"
+
+    use {
+      -- A collection of common configurations for Neovim's built-in language server client
+      "neovim/nvim-lspconfig",
+      config = [[ require('plugins/lspconfig') ]]
+    }
+
+    use {
+      "williamboman/nvim-lsp-installer",
+      config = [[ require('plugins/lsp_installer_nvim') ]],
+      requires = {
+        "neovim/nvim-lspconfig"
+      }
+    }
+
+    use {
+      -- vscode-like pictograms for neovim lsp completion items Topics
+      "onsails/lspkind-nvim",
+      config = [[ require('plugins/lspkind') ]]
+    }
+
+    --[=[
+    use {
+      -- Utility functions for getting diagnostic status and progress messages from LSP servers, for use in the Neovim statusline
+      "nvim-lua/lsp-status.nvim",
+      config = [[ require('plugins/lspstatus') ]]
+    }
+    ]=]--
+
+    use {
+      -- A completion plugin for neovim coded in Lua.
+      "hrsh7th/nvim-cmp",
+      requires = {
+        "hrsh7th/cmp-nvim-lsp", -- nvim-cmp source for neovim builtin LSP client
+        "hrsh7th/cmp-nvim-lua", -- nvim-cmp source for nvim lua
+        "hrsh7th/cmp-buffer", -- nvim-cmp source for buffer words.
+        "hrsh7th/cmp-path", -- nvim-cmp source for filesystem paths.
+        "hrsh7th/cmp-calc", -- nvim-cmp source for math calculation.
+        "saadparwaiz1/cmp_luasnip" -- luasnip completion source for nvim-cmp
+      },
+      config = [[ require('plugins/cmp') ]]
+    }
+
+    use {
+      "nvim-telescope/telescope-project.nvim"
+    }
+
+    use {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      run = "make"
+    }
+
+    use {
+      "nvim-telescope/telescope.nvim",
+      requires = {
+        "nvim-lua/plenary.nvim",
+        "BurntSushi/ripgrep"
+      },
+      config = [[ require('plugins/telescope') ]]
+    }
+
+    use {
+      -- Snippet Engine for Neovim written in Lua.
+      "L3MON4D3/LuaSnip",
+      requires = {
+        "rafamadriz/friendly-snippets" -- Snippets collection for a set of different programming languages for faster development.
+      },
+      config = [[ require('plugins/luasnip') ]]
+    }
+
+    use {
+      --  colorscheme for (neo)vim
+      "shaunsingh/nord.nvim"
+    }
+
+    use {
+      -- Nvim Treesitter configurations and abstraction layer
+      "nvim-treesitter/nvim-treesitter",
+      run = ":TSUpdate",
+      requires = {
+        "windwp/nvim-ts-autotag",
+        "p00f/nvim-ts-rainbow",
+      },
+      config = [[ require('plugins/treesitter') ]]
+    }
+
+    use {
+      "lukas-reineke/indent-blankline.nvim",
+      config = [[ require('plugins/blankline') ]]
+    }
+
+    use {
+      "tpope/vim-eunuch"
+    }
+
+    use {
+      "nvim-lualine/lualine.nvim",
+      requires = {"kyazdani42/nvim-web-devicons", opt = true},
+      config = [[ require('plugins/lualine') ]]
+    }
+
+    use {
+      "TimUntersberger/neogit",
+      requires = {"nvim-lua/plenary.nvim"},
+      config = [[ require('plugins/neogit') ]]
+    }
+
+    use {
+      "kyazdani42/nvim-tree.lua",
+      requires = "kyazdani42/nvim-web-devicons",
+      config = [[ require('plugins/nvim-tree') ]]
+    }
+
+    use {
+      "folke/zen-mode.nvim",
+      config = [[ require('plugins/zen-mode') ]]
+    }
+
+    use {
+      "ThePrimeagen/git-worktree.nvim",
+      config = [[ require('plugins/git-worktree') ]]
+    }
+
+    use {
+      "mhartington/formatter.nvim",
+      config = [[ require('plugins/formatter') ]]
+    }
+
+    use {
+      "kmonad/kmonad-vim"
+    }
+
+    if packer_bootstrap then
+      require("packer").sync()
+    end
+  end
+}
---
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,54 @@
+-- options for lsp diagnostic
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = true,
+    signs = true,
+    update_in_insert = true,
+    virtual_text = {
+        true,
+        spacing = 6,
+        --severity_limit='Error'  -- Only show virtual text on error
+    },
+  }
+)
+
+require('lspconfig').texlab.setup {
+  settings = {
+    texlab = {
+      auxDirectory = "build/pdf",
+      rootDirectory = ".",
+      chktex = {
+        onEdit = true
+      },
+      build = {
+        executable = "latexmk",
+        forwardSearchAfter = false,
+        onSave = true
+      }
+    }
+  }
+}
+
+require("lspconfig").ltex.setup({
+	settings = {
+		ltex = {
+			enabled = { "latex", "tex", "bib", "markdown" },
+			diagnosticSeverity = "information",
+			setenceCacheSize = 2000,
+			additionalRules = {
+				enablePickyRules = true,
+				motherTongue = "de",
+			},
+			trace = { server = "verbose" },
+			dictionary = {},
+			disabledRules = {},
+			hiddenFalsePositives = {},
+		},
+	},
+})
+
+-- se LSP diagnostic symbols/signs
+vim.api.nvim_command [[ sign define LspDiagnosticsSignError         text=✗ texthl=LspDiagnosticsSignError       linehl= numhl= ]]
+vim.api.nvim_command [[ sign define LspDiagnosticsSignWarning       text=⚠ texthl=LspDiagnosticsSignWarning     linehl= numhl= ]]
+vim.api.nvim_command [[ sign define LspDiagnosticsSignInformation   text= texthl=LspDiagnosticsSignInformation linehl= numhl= ]]
+vim.api.nvim_command [[ sign define LspDiagnosticsSignHint          text= texthl=LspDiagnosticsSignHint        linehl= numhl= ]]
